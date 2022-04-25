c2397dda45f1a3df506489fb80503f09fe76e9f3
Kévin Lacroix <78355350+KLacroix-Hub@users.noreply.github.com>
2022-04-25 09:33:09 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
+import logo from './logo.svg';
 import './App.css';
-import ColorPicker from './components/ColorPicker';
-import Range from './components/Range';
+import Gradient from './Component/Gradient';
+import Title from './Component/Title';
 
 function App() {
   return (
-    <div className="App">
-      <h1>CSS GRADIENT GENERATOR</h1>
-      <div className='grid'>
-      <ColorPicker />
-      <ColorPicker />
-      <ColorPicker />
-      <Range />
-      </div>
-    </div>
+    <>
+      <Title/>
+      <Gradient/>
+    </>  
   );
 }
 
